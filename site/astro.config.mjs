855bb6f66fdf9bfc6665dd530f19eff20cb62e01
rigerc <bond@bsgb.nl>
2025-08-21 01:27:05 +0200

    test

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rigerc.github.io",
-  base: "/bondreviews",
   trailingSlash: "ignore",
   integrations: [sitemap(), mdx()],
   vite: {
